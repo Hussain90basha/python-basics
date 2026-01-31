808ed92b995e9a2a2a13b221d8a739f897cf4c65
Hussain90basha <bashahussain9010@gmail.com>
2026-01-31 14:35:56 +0530

    python

--- lists.py
+++ lists.py
@@ -8,6 +8,17 @@ print(type(servers), servers, server_1, server_2)
 server_1 = servers[0]
 print("server 1 IP address:", server_1)
 
-# Slicing (Start_index:end_index +1step:size), as end_index in python is not inclusive 
-simple_slice = servers[0:5:2]
+# Slicing (Start_index:end_index +1step_size), as end_index in python is not inclusive 
+# step_size = 1 (default)
+simple_slice = servers[1:5:2]
 print(simple_slice)
+
+number = [11, 12, 13, 14, 13, 17, 18, 20]
+slice = number[1:8:2]
+print(slice)
+
+# Negative indexing
+simple_slice= servers[-1:-4:-1]
+print(simple_slice)
+
+print("length of list:", len(simple_slice))
