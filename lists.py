f6c001bda50d6798f23d1ab64e81bad49cc2ed8c
Hussain90basha <bashahussain9010@gmail.com>
2026-01-31 14:16:34 +0530

    python

--- lists.py
+++ lists.py
@@ -1,9 +1,13 @@
 server_1 = "172.10.12.1"
 server_2 = "172.10.12.2" 
 
-servers = ["172.10.12.1", "172.10.12.2", True, 123, 123.45]
+servers = ["172.10.12.1", "172.10.12.2", True, 123, 123.45, 123.456]
 print(type(servers), servers, server_1, server_2)
 
 # Python is zero index based
 server_1 = servers[0]
 print("server 1 IP address:", server_1)
+
+# Slicing (Start_index:end_index +1step:size), as end_index in python is not inclusive 
+simple_slice = servers[0:5:2]
+print(simple_slice)
