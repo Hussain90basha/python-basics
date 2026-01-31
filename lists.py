3f14bbe1e0528cd245be6fec60fe6cd5ccb10914
Hussain90basha <bashahussain9010@gmail.com>
2026-01-31 14:04:48 +0530

    python

--- lists.py
+++ lists.py
@@ -3,3 +3,7 @@ server_2 = "172.10.12.2"
 
 servers = ["172.10.12.1", "172.10.12.2", True, 123, 123.45]
 print(type(servers), servers, server_1, server_2)
+
+# Python is zero index based
+server_1 = servers[0]
+print("server 1 IP address:", server_1)
