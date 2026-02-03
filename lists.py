468b1a5cc8a874929bd980d92503d6ef5390d7ff
Hussain90basha <bashahussain9010@gmail.com>
2026-02-03 15:59:58 +0530

    python

--- lists.py
+++ lists.py
@@ -1,3 +1,6 @@
+l = []
+l = list()
+
 server_1 = "172.10.12.1"
 server_2 = "172.10.12.2" 
 
@@ -6,11 +9,16 @@ print(type(servers), servers, server_1, server_2)
 
 # Python is zero index based
 server_1 = servers[0]
-print("server 1 IP address:", server_1)
+# print("server 1 IP address:", server_1)
 
 # Slicing (Start_index:end_index +1step_size), as end_index in python is not inclusive 
 # step_size = 1 (default)
 simple_slice = servers[1:5:2]
+simple_slice = servers[1:]
+print(simple_slice)
+simple_slice = servers[:5]
+print(simple_slice)
+simple_slice = servers[:]
 print(simple_slice)
 
 number = [11, 12, 13, 14, 13, 17, 18, 20]
@@ -27,7 +35,7 @@ print("Before modify:", servers)
 servers[-3] = 1234 # Inplace operation
 print("After modification:", servers)
 
-print("list of operation:", dir(servers))
+# print("list of operation:", dir(servers))
 """
 ['append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
 """
---
+++ tuples.py
@@ -0,0 +1,5 @@
+t = ()
+t1 = tuple()
+
+sample = ("172.10.12.1", "172.10.12.2", True, 123, 123.45, 123.456)
+print(type(sample), sample)
