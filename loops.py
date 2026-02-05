4940f41bab8e2ab40335f5eb6ab614869c7715f9
Hussain90basha <bashahussain9010@gmail.com>
2026-02-05 17:05:33 +0530

    python

--- loops.py
+++ loops.py
@@ -10,17 +10,31 @@ while value < 10:
         # increment is very important
         value = value + 1
         continue
-    print(value) 
+    # print(value) 
     value = value + 1
 
-sample = ["server1", "server2", "server3", "server4"]
+sample = ["server1", "server2", "server3", "server4", 123]
 
 idx = 0
 
 while idx < len(sample):
-    print(sample[idx])
+    # print(sample[idx])
     idx += 1 # idx = idx + 1
 
 # in -> membership operator (checks whether that element is present or not)
-print(1 in sample)
-print("server1" in sample)    
+# print(1 in sample)
+# print("server1" in sample)   
+
+# idx = iterable
+# sample = iterator
+
+for ele in sample:
+    print(ele) 
+print(ele)    
+
+# access elements inside a list with index using for loop
+# range, enumerate
+print(list(range(5)))    
+
+for idx in range(len(sample)):
+    print(sample[idx])
