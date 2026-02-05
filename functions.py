ae0b795aba5de0554b54fb638b722d1f785c3d0e
Hussain90basha <bashahussain9010@gmail.com>
2026-02-05 17:50:56 +0530

    python

--- functions.py
+++ functions.py
@@ -3,7 +3,12 @@ def func_name():
 
 print(func_name())    
 
-def mult(a:int, b:int) -> int:
+def mult(a, b, *args, **kwargs):
+    # *args -> pass any no: of position arguments
+    # **kwargs -> pass any no: keyword arguments
+    print(a, b, args, kwargs)
     return a * b  # default is none
-res = mult ('3.0', 3)
+# a=4, b=5 are keyword arguments, as they contains keys and values
+# '3.0', 3 are called as positional arguments
+res = mult ('3.0', 3, 4, 5, c=10, d=20)
 print(res)
