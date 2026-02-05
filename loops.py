9e35ccbf316b28680991636e89d5856d374c5a89
Hussain90basha <bashahussain9010@gmail.com>
2026-02-05 16:32:10 +0530

    python

---
+++ loops.py
@@ -0,0 +1,11 @@
+sample = ["server1", "server2", "server3", "server4"]
+
+# for, while
+
+value = 0 
+
+while value < 10:
+    if value == 5:
+        break
+    print(value) 
+    value = value + 1
